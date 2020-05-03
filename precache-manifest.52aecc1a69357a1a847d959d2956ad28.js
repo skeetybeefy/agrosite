10f1b64435e3b0355bd1cf7a0b13e2e0ff047a14
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-03 18:47:45 +0000

    Deploy skeetybeefy/agrosite to github.com/skeetybeefy/agrosite.git:gh-pages

---
+++ precache-manifest.52aecc1a69357a1a847d959d2956ad28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36dfa5875c6027efaffd8f223054d838",
+    "url": "/agrosite/index.html"
+  },
+  {
+    "revision": "3bfb19f22742997b80bc",
+    "url": "/agrosite/static/css/main.5a33a620.chunk.css"
+  },
+  {
+    "revision": "99edaa553ab872b968f5",
+    "url": "/agrosite/static/js/2.af302968.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/agrosite/static/js/2.af302968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bfb19f22742997b80bc",
+    "url": "/agrosite/static/js/main.7559d466.chunk.js"
+  },
+  {
+    "revision": "b865abbe1841d5bcbc23",
+    "url": "/agrosite/static/js/runtime-main.83b3fd2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/agrosite/static/media/logo.5d5d9eef.svg"
+  }
+]);
